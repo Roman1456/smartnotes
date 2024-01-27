de1eb1e028b0ca816ac53f76f5d1dad56f925815
lpopivnyak <stadnykroman2010@gmail.com>
2024-01-27 19:37:46 +0200

    smartnotes12

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import json
+
 from PyQt5.QtWidgets import *
 
 app = QApplication([])
 
+notes={}
 window = QWidget()
 window.resize(700,500)
 
@@ -27,14 +30,13 @@ mine_line = QHBoxLayout()
 
 mine_line.addWidget(answer_text)
 
-h2 = QHBoxLayout
+
 h1 = QVBoxLayout()
+
 h1.addWidget(qest_lbl)
 h1.addWidget(answer_list)
 h1.addWidget(qest_btn)
-
-h2.addWidget(qest_btn1)
-
+h1.addWidget(qest_btn1)
 h1.addWidget(qest_btn2)
 
 
@@ -49,9 +51,42 @@ h1.addStretch(1)
 mine_line.addLayout(h1)
 
 
-
-
-
 window.setLayout(mine_line)
+
+def read_data():
+    global notes
+    with open("database.json","r", encoding="utf-8") as file:
+        notes = json.load(file)
+
+def write_data():
+    global notes
+    with open("database.json","w", encoding="utf-8") as file:
+        json.dump(notes,file, ensure_ascii=False,indent=4)
+read_data()
+answer_list.addItems(notes)
+
+def vmist_note():
+    name = answer_list.selectedItems()[0].text()
+    answer_text.setText(notes[name]["Вміст"])
+
+answer_list.itemClicked.connect(vmist_note)
+
+def change_note():
+    name = answer_list.selectedItems()[0].text()
+    notes[name]["Вміст"]=answer_text.toPlainText()
+    write_data()
+
+qest_btn2.clicked.connect(change_note)
+
+def add_note():
+    res, ok =QInputDialog.getText(window, "Ведення" , "Ведіть назву замітку")
+    if ok:
+        notes[res] = {
+            "Вміст": "",
+            "Теги": []
+        }
+        write_data()
+
+qest_btn.clicked.connect(add_note)
 window.show()
 app.exec()
